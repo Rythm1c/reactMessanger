8efdce4c1879d66fceba32c1e2d10d2dca4d0e2a
Rythm1c <vickkaniu0@gmail.com>
2025-04-13 12:57:33 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/",
+  base:"https://Rythm1c.github.io/ChatApp/",
   plugins: [
     react(),
     tailwindcss(),
